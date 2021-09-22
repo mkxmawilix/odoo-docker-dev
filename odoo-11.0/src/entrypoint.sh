b647fb4a2e0909ec691746ac6834a22a562f6e0a
Mkxm Awilix <mkxm@outlook.fr>
2021-09-22 09:18:39 +0200

    [FIX/IMP] Fixed a nodejs depedency and added debugpy for Odoo 11
    * Fixed a new nodejs dependency with node-gyp and nodejs-dev
        since the last build.
        There was "conflicts with broken package" during the build of
        the image  between libssl-dev and libssl1.0-dev
    * Added debugpy python lib in the Odoo11 Dockerfile
    * Updated the entrypoint.sh file to add the debugpy launch option

--- odoo-11.0/src/entrypoint.sh
+++ odoo-11.0/src/entrypoint.sh
@@ -9,6 +9,14 @@ if [ "$1" = 'odoo' ]; then
         "--db_user=$DB_USER" \
         "--db_password=$DB_PASSWORD" \
         $ODOO_EXTRA_ARGS "$@"
+elif [ "$1" = 'debug' ]; then
+    shift
+    exec /usr/bin/python3 -m debugpy --listen 0.0.0.0:3001 ./odoo-bin \
+        "--db_host=$DB_HOST" \
+        "--db_port=$DB_PORT" \
+        "--db_user=$DB_USER" \
+        "--db_password=$DB_PASSWORD" \
+        $ODOO_EXTRA_ARGS "$@"
 else
     exec "$@"
 fi
